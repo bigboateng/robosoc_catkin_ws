91d159331318c996e98ca325a5113562d6e3b742
MatOchal <mateusz.ochal@yahoo.com>
2016-03-03 18:55:58 +0000

    AStar

---
+++ src/AStar/map.py
@@ -0,0 +1,13 @@
+from astar import *
+
+class Robot(object):
+    currentX=0
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+def currentGridPosition:
+    return x,y;
+
+class AStarGridNode(AStarNode):
+    
+
